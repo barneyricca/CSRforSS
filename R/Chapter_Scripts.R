8c34915c591713088ef516b1b918c0b56f50b7a7
barneyricca <barneyricca@gmail.com>
2020-05-17 08:15:19 -0400

    Initial commit

---
+++ R/Chapter_Scripts.R
@@ -0,0 +1,13 @@
+#' test_add
+#'
+#' This function returns the sum of two numbers.
+#' @param <a> first addend
+#' @param <b> second addend
+#' @keywords add
+#' @export
+#' @examples
+#' test_add(2,3)
+
+test_add <- function(a,b) {
+  return(a+b)
+}
